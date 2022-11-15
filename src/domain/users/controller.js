d3d34f8154aabf4f96a29f8dd46005b6ae621861
Kirill Kalachev <kalachev.kirill@bk.ru>
2022-11-15 15:07:23 +0300

    Initial commit

---
+++ src/domain/users/controller.js
@@ -0,0 +1,48 @@
+const e = require('express');
+const userModel = require('./model');
+
+module.exports = {
+	getAll: (req, res) => {
+		return res.render('users.hbs', {
+			users: userModel.getAll()
+		})
+	},
+	create: (req, res) => {
+		try {
+			const {age, username} = req.body;
+	
+			if(!age || !username) {
+				throw new Error ('Не указан username или возраст');
+			}
+	
+			userModel.create({age, username});
+	
+			return res.redirect('/users');
+		}
+		catch (e){
+			return res.render('users-error.hbs', {
+				message: e.message
+			})
+		}
+	},
+	removeById: (req, res) => {
+		try {
+			const id = req.querry.id;
+
+			if(!id) {
+				throw new Error('id не указан');
+			}
+
+			userModel.removeById({id});
+
+			res.render('users-view.hbs', {
+				users: userModel.getAll()
+			})
+		}
+		catch (e){
+			return res.remder('users-error.hbs', {
+				message: e.message
+			})
+		}
+	},
+}
